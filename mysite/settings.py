72957a81758819b2aaf04b2bae4e3ee56b557008
tatene9912 <t.atene0l4o1v2e@gmail.com>
2024-12-03 19:07:27 +0900

    heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -109,10 +111,9 @@ ACCOUNT_FORMS = {
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+    )
 }
 
 
