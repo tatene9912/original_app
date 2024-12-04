c8dda822c646844cf179facb495c8d0512562841
tatene9912 <t.atene0l4o1v2e@gmail.com>
2024-12-04 11:05:10 +0900

    heroku

--- GOODstime/models.py
+++ GOODstime/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from accounts.models import User
 from django.conf import settings
 from django.urls import reverse
+from cloudinary.models import CloudinaryField
 
 transaction_CHOICES = [
     (1, '交換'),
@@ -31,9 +32,9 @@ class Post(models.Model):
     type_transfer = models.PositiveIntegerField(verbose_name='郵送or手渡し',choices=transfer_CHOICES)
     match_user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='マッチングユーザー', related_name='match_posts',null=True, blank=True)
     status = models.CharField(verbose_name='ステータス', max_length=20, choices=STATUS_CHOICES, default='unmatched', blank=True, null=True)
-    image1 = models.ImageField(verbose_name='画像1')
-    image2 = models.ImageField(verbose_name='画像2', blank=True)
-    image3 = models.ImageField(verbose_name='画像3', blank=True)
+    image1 = CloudinaryField(verbose_name='画像1')
+    image2 = CloudinaryField(verbose_name='画像2', blank=True)
+    image3 = CloudinaryField(verbose_name='画像3', blank=True)
     delete_flg = models.BooleanField(verbose_name='削除フラグ', default=False, null=True, blank=True)
     created_at = models.DateTimeField(verbose_name='作成日時', auto_now_add=True)
     updated_at = models.DateTimeField(verbose_name='更新日時', auto_now=True)
